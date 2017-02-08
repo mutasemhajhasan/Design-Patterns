3fbcc9a79528a2cf4d55c50a1711e56cdfc569c2
Mutasem Haj Hasan <mhh.sy@hotmail.com>
2017-02-08 04:29:11 +0200

    initial

---
+++ Strategy/src/com/mutasem/strategy/abstraction/Context.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mutasem.strategy.abstraction;
+
+/**
+ *
+ * @author Mutasem
+ */
+public class Context {
+    private Strategy strategy;
+
+    public Context(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public Strategy getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+    public void work(){
+    strategy.execute();
+    }
+}
---
+++ Strategy/src/com/mutasem/strategy/abstraction/Strategy1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mutasem.strategy.abstraction;
+
+/**
+ *
+ * @author Mutasem
+ */
+public class Strategy1 extends Strategy{
+
+    @Override
+    public void execute() {
+        System.out.println("Strategy1.execute()");
+    }
+    
+}
